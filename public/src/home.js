d398a128cc481c6333be1e23a65e3ce94dd285ed
Sophie Goldstein <110262769+Sophgolds@users.noreply.github.com>
2022-09-07 16:36:00 -0400

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -19,16 +19,24 @@ function getBooksBorrowedCount(books) {
 }
 
 function getMostCommonGenres(books) {
+  // What are the top five most common genres?
+  // create a const varible for books that map the genre
   const genres = books.map((book) => book.genre)
+  // need an empty array
   const topFive = []
+  //map over the GENRES varible 
   genres.map((genre) => {
+    // Nees a varible to check each genre to see if it exsists
     const location = topFive.findIndex((element) => element.name === genre);
+    // exsists & >= 0 want to increase by 1
     if (location >= 0){
       topFive[location].count = topFive[location].count +1;
+      // else want to push a new array and count 1
 } else {
   topFive.push({name: genre, count: 1});
       }
     });
+  // will sort from lowest to highest
     topFive.sort((a, b) => b.count - a.count);
     if (topFive.length > 5) {
       return topFive.slice(0, 5);
@@ -37,14 +45,17 @@ function getMostCommonGenres(books) {
 }
 
 function getMostPopularBooks(books) {
+  // create a const with an empty array
   let popular = [];
+  //
   const borrows = books.reduce((acc, book) => {
     popular.push({ name: book.title, count: book.borrows.length });
   }, []);
 
   return mostPopular(popular);
 }
-
+// function sorts the popular array
+// the slice --> will create a new array with the objects selected
 function mostPopular(array) {
   let popularBooks = array
     .sort((countA, countB) => (countA.count < countB.count ? 1 : -1))
