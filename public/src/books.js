d66749dcd997dd392b475cdd2f65729d8a1e8f72
Sophie Goldstein <110262769+Sophgolds@users.noreply.github.com>
2022-09-07 16:13:16 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,22 +1,30 @@
 function findAuthorById(authors, id) {
+  // whats to FIND by author
   const authorById = authors.find((authors) => authors.id === id)
   return authorById;
 }
 
 function findBookById(books, id) {
+  // Want to FIND by book ID
   const bookById = books.find((books) => books.id === id)
   return bookById;
 }
 
 function partitionBooksByBorrowedStatus(books) {
+  // What is the book status?
+  // was the book returned?
   const returned = books.filter((book) => book.borrows[0].returned)
+  // was the book brought back?
   const borrowed = books.filter((book) => !book.borrows[0].returned);
   return [borrowed, returned]
 }
 
 function getBorrowersForBook(book, accounts) {
+  // want to know who is borrowing the book
     const { borrows } = book;
+  // need the account id to know who has the potenial book out
     const renters = borrows.map(({ id, returned })=> {
+      // account needs to match account id
       const account = accounts.find(account => account.id === id);
       return {
         ...account,
